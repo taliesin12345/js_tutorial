9c2c2c3b5a6daedf0fe57c4bd6e840ea9b697fd6
Andrex Lorenzini <andrea.lorenzini4@gmail.com>
2021-04-14 17:19:07 +0200

    Chpt 9 - A button and a form to trigger EventListeners

--- bundle.js
+++ bundle.js
@@ -1,14 +1,31 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 let Phrase = require("my-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
+function palindromeTester() {
+  let string = prompt("Please enter a string for palindrome testing:");
+  let phrase = new Phrase(string);
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`)
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`)
+  if (phrase.palindrome()) {
+    alert(`"${phrase.content}" is a palindrome!`);
+  } else {
+    alert(`"${phrase.content}" is not a palindrome.`)
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  let button = document.querySelector("#palindromeTester");
+  button.addEventListener("click", function() {
+    palindromeTester();
+  });
+});
+
+// Exercise (a form plus a Submit button instead of a simple button):
+document.addEventListener("DOMContentLoaded", function() {
+  let form = document.querySelector("#palindromeTesterBis");
+  form.addEventListener("submit", function() {
+    palindromeTester();
+  });
+});
 
 },{"my-palindrome":2}],2:[function(require,module,exports){
 module.exports = Phrase;
--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 let Phrase = require("my-palindrome");
 
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
+function palindromeTester() {
+  let string = prompt("Please enter a string for palindrome testing:");
+  let phrase = new Phrase(string);
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`)
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`)
+  if (phrase.palindrome()) {
+    alert(`"${phrase.content}" is a palindrome!`);
+  } else {
+    alert(`"${phrase.content}" is not a palindrome.`)
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  let button = document.querySelector("#palindromeTester");
+  button.addEventListener("click", function() {
+    palindromeTester();
+  });
+});
+
+// Exercise (a form plus a Submit button instead of a simple button):
+document.addEventListener("DOMContentLoaded", function() {
+  let form = document.querySelector("#palindromeTesterBis");
+  form.addEventListener("submit", function() {
+    palindromeTester();
+  });
+});
