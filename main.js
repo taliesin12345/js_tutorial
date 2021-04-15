06c858f51c77d98d760d319497284173683eee4c
Andrex Lorenzini <andrea.lorenzini4@gmail.com>
2021-04-15 12:37:36 +0200

    Chpt 9 - Dynamic HTML for prompt result

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ let Phrase = require("my-palindrome");
 function palindromeTester() {
   let string = prompt("Please enter a string for palindrome testing:");
   let phrase = new Phrase(string);
+  let palindromeResult = document.querySelector("#palindromeResult");
 
   if (phrase.palindrome()) {
-    alert(`"${phrase.content}" is a palindrome!`);
+    palindromeResult.innerHTML = `"<strong>${phrase.content}</strong>" is a palindrome!`;
   } else {
-    alert(`"${phrase.content}" is not a palindrome.`)
+    palindromeResult.innerHTML = `"<strong>${phrase.content}</strong>" is not a palindrome.`;
   }
 }
 
@@ -19,9 +20,9 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 // Exercise (a form plus a Submit button instead of a simple button):
-document.addEventListener("DOMContentLoaded", function() {
-  let form = document.querySelector("#palindromeTesterBis");
-  form.addEventListener("submit", function() {
-    palindromeTester();
-  });
-});
+// document.addEventListener("DOMContentLoaded", function() {
+//   let form = document.querySelector("#palindromeTesterBis");
+//   form.addEventListener("submit", function() {
+//     palindromeTester();
+//   });
+// });
